14627915a58192fd91a0972bf6b65bd89022a29c
8exor <85976444+8exor@users.noreply.github.com>
2024-10-24 11:06:02 +0530

    Update UserRoutes.js

--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import UserController from "../Controllers/UserController.js";
+//import UserController from "../Controllers/UserController.js";
 
 let router = express.Router();
 
-router.post('/register', UserController.register);
+//router.post('/register', UserController.register);
 
 
 export const UserRoutes = router;
